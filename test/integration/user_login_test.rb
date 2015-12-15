c856ef68a37460ff25be6a455608dd9ab8bee239
ShannonPaige <spaige@gmail.com>
2015-12-14 19:40:02 -0700

    Add twitter oauth login

--- test/integration/user_login_test.rb
+++ test/integration/user_login_test.rb
@@ -9,4 +9,13 @@ class UserLoginTest < ActionDispatch::IntegrationTest
     assert page.has_content?("Shannon")
     assert page.has_link?("Logout")
   end
+
+  # test "twitter user can login using their twitter profile" do
+  #   visit '/'
+  #   assert_equal 200, page.status_code
+  #   click_link "Login"
+  #   assert_equal "/", current_path
+  #   assert page.has_content?("Shannon")
+  #   assert page.has_link?("Logout")
+  # end
 end

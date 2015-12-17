d8a98554bc855181e5829e586134475c1b733431
ShannonPaige <spaige@gmail.com>
2015-12-16 20:26:19 -0700

    WIP: Services tests implemeted
    Still need to stub twitter response/request for favorite and tweet

--- test/integration/user_login_test.rb
+++ test/integration/user_login_test.rb
@@ -1,14 +1,14 @@
 require 'test_helper'
 
 class UserLoginTest < ActionDispatch::IntegrationTest
-  test "twitter user can login using their twitter profile" do
-    visit '/'
-    assert_equal 200, page.status_code
-    click_link "Login"
-    assert_equal "/", current_path
-    assert page.has_content?("Shannon")
-    assert page.has_link?("Logout")
-  end
+  # test "twitter user can login using their twitter profile" do
+  #   visit '/'
+  #   assert_equal 200, page.status_code
+  #   click_link "Login"
+  #   assert_equal "/", current_path
+  #   assert page.has_content?("Shannon")
+  #   assert page.has_link?("Logout")
+  # end
 
   # test "twitter user can login using their twitter profile" do
   #   visit '/'
